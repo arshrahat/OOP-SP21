42214aed122cb2ff23a0fed7f5895867539135b2
arshrahat <80713539+arshrahat@users.noreply.github.com>
2021-04-20 21:29:56 +0500

    Update Computerunner.java

--- Computerunner.java
+++ Computerunner.java
@@ -4,8 +4,8 @@ package computer;
 
 public class Computerunner {
     public static void main(String[] args) {
-        Laptop lap=new Laptop(6,64,32,10,30,14,7,40);
-        lap.Display();
+        Laptop l=new Laptop(6,64,32,10,30,14,7,40);
+        l.Display();
     }
     
 }
